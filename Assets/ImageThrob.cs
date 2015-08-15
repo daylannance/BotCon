dfcb3f9bc3e2d53dfaa42b3d6011454095214055
daylannance <daylannance@gmail.com>
2015-08-14 21:47:54 -0400

    Adds blinking to modal window image

---
+++ Assets/ImageThrob.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageThrob : MonoBehaviour {
+    public Image image;
+    public float throbsPerSecond;
+    float currentRadians;
+    float radiansPerUpdate;
+	// Use this for initialization
+	void Awake () {
+        radiansPerUpdate = ((Mathf.PI * 2) / throbsPerSecond) * Time.fixedDeltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void FixedUpdate()
+    {
+        currentRadians += radiansPerUpdate;
+        Color color = image.color;
+        color.a = Mathf.Sin(currentRadians);
+        image.color = color;
+    }
+}
